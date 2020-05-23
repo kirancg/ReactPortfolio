f88a2f87b67c92c59e700ef97768d2dc76a7705e
Kiran_Kumar_Gangadharaiah <kirancg93@gmail.com>
2020-05-23 00:55:57 -0400

    Added first part of resume Component

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,10 +1,55 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+//import Toolbar from '@material-ui/core/ToolBar';
+import {
+    Typography,
+    Box
+} from '@material-ui/core';
+import Navbar from './Navbar';
 
- const Resume = () => {
+const useStyles = makeStyles(theme => ({
+    mainContainer: {
+        background: "#233"
+    },
+    timeLine: {
+        position: "relative",
+        padding: "1rem",
+        margin: "0 auto",
+        "&:before": {
+            content: "''",
+            position: "absolute",
+            height: "100%",
+            border: "1px solid tan",
+            right: "40px",
+            top: 0
+        },
+        "&:after": {
+            content: "''",
+            display: "table",
+            clear: "both"
+        },
+        [theme.breakpoints.up("md")]: {
+            padding: "2rem",
+            "&:before": {
+                left: "calc(50%-1px)",
+                right: "auto"
+            }
+         }
+    }
+}));
+
+const Resume = () => {
+    const classes = useStyles();
     return (
-        <div>
-            Hello From Resume
-        </div>
+        <>
+            <Navbar />
+            <Box component="header" className={classes.mainContainer}>
+                <Typography variant="h4" align="center">Work Experience</Typography>
+                <Box component="div" className={classes.timeLine}>
+                <Typography variant="h2" align="center">2013</Typography>
+                </Box>
+            </Box>
+        </>
     )
 }
 
