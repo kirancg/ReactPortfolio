8dea91ae23ca4232ac6779a4c7b0a8c50f576ac5
Kiran_Kumar_Gangadharaiah <kirancg93@gmail.com>
2020-05-22 22:19:50 -0400

    Added Animations

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,69 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Header from './Header';
+import Particles from 'react-particles-js';
+import { makeStyles } from '@material-ui/core/styles';
+
+//css styles
+
+const useStyles = makeStyles({
+    particlesCanva: {
+        position: "absolute"
+    }
+})
 
 const Home = () => {
+
+    const classes = useStyles();
+
     return (
         <div>
+            <>
             <Navbar />
-            <Header />
+                <Header />
+                <Particles
+                    canvasClassName={classes.particlesCanva}
+                    paramas={{
+                        particles: {
+                            number: {
+                                value: 45,
+                                density: {
+                                    enable: true,
+                                    value_area: 900
+                                }
+                            },
+                            shape: {
+                                type: "star",
+                                stroke: {
+                                    width: 1,
+                                    color: 'tomato'
+                                }
+                            },
+                            size: {
+                                value: 8,
+                                random: true,
+                                // animation: false,
+                                anim: {
+                                    enable: true,
+                                    speed: 8,
+                                    size_min: 0.1,
+                                    sync: true
+                                }
+                            },
+                            opacity: {
+                                values: 1,
+                                random: true,
+                                anim: {
+                                    enable: true,
+                                    speed: 1,
+                                    opacity_min: 0.1,
+                                    sync: true
+                                }
+                            }
+                        }
+                    }}
+                />
+            </>
         </div>
     )
 }
